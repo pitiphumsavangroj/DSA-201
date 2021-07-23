f7273f1324d3ce1938d3cc2be8d614b97cd979c1
pitiphumsavangroj <87828522+pitiphumsavangroj@users.noreply.github.com>
2021-07-23 10:31:07 +0700

    Delete assignment.py

--- assignment.py
+++
@@ -1,7 +0,0 @@
-url = 'https://raw.githubusercontent.com/hjorturlarsen/IMDB-top-100/master/data/movies.json'
-top = pd.read_json(url)
-del top['id']
-top[['rank', 'title']].rename(columns = {
-    'rank': 'Rank',
-    'title': 'Movie Title'
-}).dropna()
